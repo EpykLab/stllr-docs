fc5c7008161014e2ac7b298de7c8f574652ca2e6
DavidHoenisch <dh1689@pm.me>
2026-08-17 08:37:38 -0700

    GENESIS: let there be light

--- .svelte-kit/generated/client/nodes/1.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../node_modules/.pnpm/@sveltejs+kit@2.53.3_@sveltejs+vite-plugin-svelte@6.2.4_svelte@5.53.5_vite@7.3.2_@types_0311cc60b25e8fcc1f92201b78a32caa/node_modules/@sveltejs/kit/src/runtime/components/svelte-5/error.svelte";
